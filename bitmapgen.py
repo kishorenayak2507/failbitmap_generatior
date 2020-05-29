1058e8ca5675ebca9becc0613a0d2eec1be886bf
kishorenayak2507 <kishorenayakm93@gmail.com>
2020-05-29 22:40:13 +0530

    cvs file formate change

--- bitmapgen.py
+++ bitmapgen.py
@@ -2,7 +2,7 @@ import csv
 
 scp_version = 'MAB000125'
 
-with open('failure.cvs', 'w', newline='') as fail:
+with open('failure.csv', 'w', newline='') as fail:
     fieldnames = ['Failurebitmapof' + scp_version]
     DictWriter = csv.DictWriter(fail, fieldnames=fieldnames)
 
